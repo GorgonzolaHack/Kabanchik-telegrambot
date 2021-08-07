9d4cf17ac36c4c654f7e9a0c67f113a60d9848da
cairo2000 dev <56520379+cairo2000@users.noreply.github.com>
2021-08-07 18:56:08 +0300

    Create heeh.py

---
+++ heeh.py
@@ -0,0 +1,53 @@
+# -*- coding: utf8 -*-
+#1030811973
+import telebot
+import requests
+from fake_useragent import UserAgent
+from time import sleep
+from telebot import types
+
+UserAgent().chrome
+
+menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
+menu.add("Для всех","Для Алины",row_width=3)
+
+bot = telebot.TeleBot("1935789229:AAEumsN5AhBQqILVcV2QJCzK-MWfx5tpWSQ")
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    bot.send_message(message.chat.id, "Привет! Я Кабанчик онлайн! Я выучил таджицкий!",reply_markup=menu)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Для всех":
+        if message.chat.id == 1030811973:
+            bot.send_message(message.chat.id, "Ты 2! Меня не заскамишь!",reply_markup=menu)
+        else:
+            bot.send_message(message.chat.id, "Я кабанчик онлайн! Отправляй мне слово или предложение, а я переведу его на Таджицкий")
+    if message.text == "Для Алины":
+        if message.chat.id != 1030811973:
+            bot.send_message(message.chat.id, "Ты 1! Меня не заскамишь!",reply_markup=menu)
+        else:
+            bot.send_message(message.chat.id, "Идентификация Борна прошла успешно!")
+            sleep(1)
+            bot.send_message(message.chat.id, "Я кабанчик онлайн! Отправляй мне слово или предложение, а я переведу его на Таджицкий")
+            sleep(1)
+            bot.send_message(message.chat.id, "А что ты ждала здесь? Ахах Вас заскамили")
+#            sleep(1)
+#            bot.send_message(message.chat.id, "И еще раз повторю, что можно было просто спокойно попросить меня так не шутить. Я так же как и ты знаю русский)")
+    else:
+        
+        
+#        translator= Translator(to_lang="tg")
+#        translation = translator.translate(message.text)
+        if message.text == "1" or message.text == "2":
+            return
+        page = (requests.get("https://ru.contdict.com/перевод/русский-таджикский/"+message.text, headers={'User-Agent': UserAgent().chrome})).text
+        start = 68+(page.find('id="translit"'))
+        end = page.find('<',start)
+        translation = page[start:end]
+        if translation == "":
+            bot.send_message(message.chat.id, "Не нашёл перевода(")
+            return
+        bot.send_message(message.chat.id, translation)
+bot.polling()
